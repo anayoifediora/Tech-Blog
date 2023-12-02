109f91f3e775b7658f7f1064ef7e3bce4d4f85a0
Kanayo <anayoifediora@live.com>
2023-12-02 22:13:29 +1030

    completed the article form

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -16,18 +16,35 @@ const Dashboard = () => {
     console.log(articles)
 
     return (
-        <div>
+        <div className = 'dashboard'>
             <section className = 'user-articles-section'>
+                <h2 style={{fontSize: '1.7rem'}}>{`${Auth.getProfile().data.username}'s Articles`}</h2>
                 {articles.map((article) => 
                     <div key={article._id} className= 'user-article'>
                         <Link to ={`/articles/${article._id}`}><p>{article.title}</p></Link>
-                        
                         <div>
                             <button>Update</button>
                             <button>Delete</button>
                         </div>
                     </div>
                 )}
+            </section>
+            <section className = "create-article">
+                <h2 style={{fontSize: '1.7rem'}}>Write Article</h2>
+                <form className = 'article-form'>
+                    <div>
+                        <label >Title</label>
+                        <input style={{ width: '50vw'}}/>
+                    </div>
+                    <div>
+                        <label>Description</label>
+                        <textarea
+                        className = 'article-text'></textarea>
+                    </div>
+                    <button>Submit</button>
+
+                </form>
+
             </section>
         </div>
     )
